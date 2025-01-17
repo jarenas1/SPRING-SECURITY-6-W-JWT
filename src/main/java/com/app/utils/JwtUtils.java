d40690fc82ff8a314058b8134f4f79a01dd022b1
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-01-16 19:17:10 -0500

    create. other methods to work with jwt, like extract a specific claim

--- src/main/java/com/app/utils/JwtUtils.java
+++ src/main/java/com/app/utils/JwtUtils.java
@@ -1,13 +1,18 @@
 package com.app.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -42,5 +47,39 @@ public class JwtUtils {
                 .withNotBefore(new Date(System.currentTimeMillis())) //Tiempo despues de la generacion en el que el token sera valido, ej, se genera a la una pero sera valido 2 horas depspues, debe ser en MS
                 .sign(algorithm); //Pasamos el algoritmo de encriptacion con la firma q creamos arriba
         return jwtToken;
+    }
+
+    //verificar jwt o lanzar un error y decodificarlo
+    public DecodedJWT validateToken(String token){
+        try{
+            //algoritmo con que encriptamos el token
+            Algorithm algorithm = Algorithm.HMAC256(jwtPrivateKey);
+             //Verificar jwt
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.jwtUserGenerator) //usuario que creamos en el properties
+                    .build();
+            //decodificar token
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+
+        }catch (JWTVerificationException e){ //excepcion que soltara si el token no es valido
+            throw new JWTVerificationException("Invalid token, no authorized");
+        }
+    }
+
+    //extraer username del token
+    public String extractUsername(DecodedJWT decodedJWT){
+        //extraemos el usuario asignado a este token
+        return decodedJWT.getSubject().toString();
+    }
+
+    //metodo para exraer un claim especifico
+    public Claim getSpecificClamin(DecodedJWT decodedJWT, String claimName){
+        return decodedJWT.getClaim(claimName);
+    }
+
+    //devolver todos los claims
+    public Map<String, Claim> getAllClaims(DecodedJWT decodedJWT){
+        return decodedJWT.getClaims();
     }
 }
